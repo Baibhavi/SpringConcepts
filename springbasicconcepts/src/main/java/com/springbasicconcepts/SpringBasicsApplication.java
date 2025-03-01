e6530d7df2e59126c076859b4c36acb82aae2a98
Baibhavi <baibhavipandey@gmail.com>
2025-03-01 21:17:29 +0530

    Added: Usecase-2 SpringBoot Logger

--- springbasicconcepts/src/main/java/com/springbasicconcepts/SpringBasicsApplication.java
+++ springbasicconcepts/src/main/java/com/springbasicconcepts/SpringBasicsApplication.java
@@ -1,14 +1,21 @@
 package com.springbasicconcepts;
 
+import com.springbasicconcepts.component.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
-
+	public static final Logger logger = LoggerFactory.getLogger(SpringBasicsApplication.class);
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept");
-		SpringApplication.run(SpringBasicsApplication.class, args);
+		logger.debug("Welcome to Spring Concept Demo");
+		ApplicationContext context = SpringApplication.run(SpringBasicsApplication.class, args);
+		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
+		logger.debug("\n*** Example Using @Autowire annotation on property ***");
+
 	}
 
 }

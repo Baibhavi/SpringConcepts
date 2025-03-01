304bca1a252bc4bd0e88cccc7ce231ba66cada9a
Baibhavi <baibhavipandey@gmail.com>
2025-03-01 21:03:06 +0530

    Added: SpringBoot Basic Application

---
+++ springbasicconcepts/src/main/java/com/springbasicconcepts/SpringBasicsApplication.java
@@ -0,0 +1,14 @@
+package com.springbasicconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Spring Concept");
+		SpringApplication.run(SpringBasicsApplication.class, args);
+	}
+
+}
